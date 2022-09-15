7e85713a23eb8a349d012e0792cc93e67155b665
ainura-z <aynura_zakirova@mail.ru>
2022-09-15 17:26:50 +0300

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,26 @@
+from Task1 import decorator_1
+import random
+
+@decorator_1
+def func():
+    print("I am ready to Start")
+    result = 0
+    n = random.randint(10, 751)
+    for i in range(n):
+        result += (i ** 2)
+
+
+@decorator_1
+def funx(n=2, m=5):
+    print("I am ready to do serious stuff")
+    max_val = float('-inf')
+    n = random.randint(10,751)
+    res = [pow(i,2) for i in range(n)]
+    for i in res:
+        if i > max_val:
+            max_val = i
+
+if __name__ == '__main__':
+    func()
+    func()
+    funx()
